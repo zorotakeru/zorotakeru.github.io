d81c0dbd6b62fbe2ade54496bd7a882da581bab4
zorotakeru <helmy18.hhh@gmail.com>
2023-07-28 14:51:05 +0700

    3

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mbelewew</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header>
+        #D4D6D6, #DFB5A4, #D81E29, #C46D63, #000001
+    </header>
+    <div id="root"></div>
+    <script src="main.js"></script>
 </body>
 </html>
